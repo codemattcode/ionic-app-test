51d692e834a1d0762e7547654677ddd8444c6682
codemattcode <codemattcode@gmail.com>
2017-05-14 22:58:09 +0100

    add custom definition to shop push navigation option

--- src/pages/shop/shop.ts
+++ src/pages/shop/shop.ts
@@ -10,6 +10,10 @@ export class ShopPage {
   constructor(private navCtrl: NavController){}
 
   onBuy(productData: {name:string, quantity: number}){
-    this.navCtrl.push(BuyoutPage, productData);
+    this.navCtrl.push(BuyoutPage, productData,{
+      direction: 'back',
+      duration: 500,
+      easing: 'ease-out'
+    });
   }
 }
